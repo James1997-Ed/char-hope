8078abe65961cf75133433a5ffd9e018e2eff1ca
Princess B.  Washington <112123284+princesswG@users.noreply.github.com>
2023-05-30 17:04:12 +0000

    new changes

---
+++ pages/ourAchievements.js
@@ -0,0 +1,7 @@
+export default function ourAchievements(){
+    return(
+        <div>
+          <div className="mt-32 sm:text-4xl text-2xl text-center font-bold"><h1>Our Achievements</h1>  </div>
+        </div>
+    )
+}
